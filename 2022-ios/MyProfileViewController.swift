a572f72316a1a2f41df20bc0114b2d34b98f8031
alexis <alexis06cs>
2022-07-17 03:56:00 -0400

    update

---
+++ 2022-ios/MyProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MyProfileViewController.swift
+//  2022-ios
+//
+//  Created by user190700 on 7/17/22.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseStorage
+import FirebaseAuth
+
+class MyProfileViewController: UIViewController {
+    
+    @IBOutlet weak var namelbl: UILabel!
+    @IBOutlet weak var lastNamelbl: UILabel!
+    @IBOutlet weak var correolbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
