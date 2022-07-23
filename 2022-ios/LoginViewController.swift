e2121bc139726f9804b0d3f7e59ad1f7dfbd01b3
alexis <alexis06cs>
2022-07-23 05:29:48 -0400

    update

--- 2022-ios/LoginViewController.swift
+++ 2022-ios/LoginViewController.swift
@@ -46,7 +46,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         emailTextF.delegate = self
         passwordTextF.delegate = self
 
-        
+         
         emailTextF.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
         passwordTextF.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
 
